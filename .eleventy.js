2fbb92b892563f3b58d7d2285c5c6a1179573732
Bryan Robinson <bryanlrobinson@gmail.com>
2019-09-04 14:45:37 -0500

    Adds filter for related content

--- .eleventy.js
+++ .eleventy.js
@@ -22,6 +22,23 @@ module.exports = function(config) {
         return collection.getFilteredByTag('posts').reverse();
     });
 
+    config.addFilter("related", function(items, categorizeBy, categoryArray, threshold = 1, excludedSlug = "") {
+      let posts = Array.from(items).filter(item => {
+        return item.fileSlug != excludedSlug // removes "excluded item" aka the current item
+      });
+
+      let filteredPosts = posts.filter(item => {
+        let matchedCount = 0;
+        item.data[categorizeBy].forEach(cat => {
+          if (categoryArray.includes(cat)) {
+            return matchedCount++
+          }
+        });
+        return (matchedCount >= threshold);
+      })
+      return filteredPosts
+    });
+
     config.addCollection('categories', collection => {
       let categories = [];
       let sortedPosts = [];
